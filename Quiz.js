4337444bff8a96e2e1d825a20f962752f0d8cec5
CarrieJulie <85888302+CarrieJulie@users.noreply.github.com>
2021-08-11 12:23:50 -0400

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,70 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+    //write code to change the background color here
+    background("yellow");
+    //write code to show a heading for showing the result of Quiz
+    this.title = createElement("h1");
+    this.title.html("Quiz Answers");
+    this.title.position(350, 0);
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+    //write condition to check if contestantInfor is not undefined
+    if (allContestants !== undefined)
+    {
+      fill("blue");
+      textSize(20);
+      text("*Note: Contestants who answered correct are in Green!", 130, 230);
+    }
+    //write code to highlight contest who answered correctly
+
+    for (var plr in allContestants)
+    {
+      var correctAns = "2";
+      //var conName = allContestants[plr].name;
+      //var conOpt = allContestants[plr].answer;
+      
+      if (correctAns === allContestants[plr].answer)
+      {
+        fill("green");
+        textSize(20);
+        text(allContestants[plr].name + " : " + allContestants[plr].answer, 130, 260);
+      } else {
+        fill("red");
+        textSize(20);
+        text(allContestants[plr].name + " : " + allContestants[plr].answer, 130, 290);
+      }
+    }
+  }
+
+}
